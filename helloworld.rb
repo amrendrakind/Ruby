556d410e31a3ec404a5e9333bb21ed88a95209b3
amrendrakind <vision2010@gmail.com>
2022-06-01 10:02:38 +0530

    Ruby in progress

--- helloworld.rb
+++ helloworld.rb
@@ -1,1 +1,16 @@
 puts "Hello Ruby !" 
+puts("Unary operator")
+puts(~5)
+puts(~-5)
+puts(!true)
+puts(!false)
+
+puts("Add of 10 and 20")
+puts(10+20)
+puts("Substraction of 10 from 30")
+puts(30-10)
+
+puts("Multiplication of 10 and 20")
+puts(10*20)
+puts("Division of 50 by 10")
+puts(50/10)
