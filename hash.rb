bea6616a535e5916412a9bfb06ea07d6e19bba15
amrendrakind <vision2010@gmail.com>
2022-06-17 17:02:38 +0530

    Hash and proc

--- hash.rb
+++ hash.rb
@@ -1,3 +1,14 @@
 empty_hash = Hash.new
 default_hash = Hash.new(1)
 hackerrank = {"simmy" => 100, "vivmbbs" => 200}
+
+# user = {"viv" : 10, "simmy" : 20, "sp2hari" : 30}
+def iter_hash(hash)
+    # your code here
+    hash.each do |key, value|
+        puts key
+        puts value
+    end
+end
+
+iter_hash(hackerrank)
