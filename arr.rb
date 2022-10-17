963188591c6d8a143b0f32303423e61688798678
amrendrakind <vision2010@gmail.com>
2022-10-17 22:13:24 +0530

    Array printing odd and even

--- arr.rb
+++ arr.rb
@@ -1,2 +1,11 @@
 a = 1..100
 a.each {|item| print item.to_s + " "}
+puts
+puts
+
+z = (1..100).to_a.shuffle
+
+z.select {|number| print number.to_s + " "  if number.even? }
+puts
+puts
+z.select {|number| print number.to_s + " "  if number.odd? }
