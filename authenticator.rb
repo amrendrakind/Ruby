11b6e209a3279d9aad655cbc4d0082fca0db4e6a
amrendrakind <vision2010@gmail.com>
2022-10-20 03:42:49 +0530

    Add: User authenticator!

---
+++ authenticator.rb
@@ -0,0 +1,15 @@
+ users = [
+            {username: "rupam", password: "password1"},
+            {username: "nilesh", password: "password2"},
+            {username: "aarohi", password: "password3"},
+            {username: "Lalita", password: "password4"},
+            {username: "munni", password: "password5"}
+         ]
+puts "Welcome to authenticator!!"
+
+20.times {print '-'}
+puts
+
+puts "This program will take input from the user and compare password"
+puts "If password is correct then, you will get back the user object"
+
