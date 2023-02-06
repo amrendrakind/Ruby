7818b681b29b5c8c9d58ca0a2b4248254ddd0713
amrendrakind <vision2010@gmail.com>
2023-02-06 19:35:15 +0530

    Add books

---
+++ freeze.rb
@@ -0,0 +1,7 @@
+array = [1,2,3,4,5]
+p array
+array<<6
+p array
+array.include?(4)
+array.freeze
+p array
---
+++ sum.rb
@@ -0,0 +1,7 @@
+print "Please enter first number "
+num1 = gets
+print "Please enter second number "
+num2 = gets
+sum = num1.to_i + num2.to_i
+print "the result is " 
+puts sum
