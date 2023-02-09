b36b27c6f788a043c2b624e859d6e9d8dec03a28
amrendrakind <vision2010@gmail.com>
2023-02-09 18:23:27 +0530

    Circle

---
+++ circle.rb
@@ -0,0 +1,6 @@
+print "Enter the radius of the circular candy: "
+radius = gets.chomp.to_f
+perimeter = 2*3.14*radius
+puts "The perimeter of the candy is #{perimeter}"
+area = 3.14*radius*radius
+puts "The area of candy is #{area}"
