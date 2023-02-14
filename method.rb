f1b424e909b3472462d1fefabb5f8fe6250e0469
amrendrakind <vision2010@gmail.com>
2023-02-14 21:46:58 +0530

    Methos manipulation

--- method.rb
+++ method.rb
@@ -9,3 +9,19 @@ def percentage_of_value a, b=50
 end
 percentage_of_value 10
 percentage_of_value 10, 70
+
+def print_value a=50, b=100, c
+    puts "value of a is #{a}"
+    puts "value of b is #{b}"
+    puts "value of c is #{c}"
+end
+
+print_value 1, 2
+puts
+def print_values a=50, b=100, c
+    puts "value of a is #{a}"
+    puts "value of b is #{b}"
+    puts "value of c is #{c}"
+end
+
+print_values 1,2,3
