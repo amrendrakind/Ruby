69808998fbdae1012edf1c7dd6897cd3c4ea3e70
amrendrakind <vision2010@gmail.com>
2023-02-14 21:36:44 +0530

    Methos manipulation

---
+++ method.rb
@@ -0,0 +1,11 @@
+def add_two_numbers a,b
+    puts "Sum of two number is #{a+b}"
+end
+
+add_two_numbers(10,20)
+
+def percentage_of_value a, b=50
+    puts "Percentge value is #{a*b/100}"
+end
+percentage_of_value 10
+percentage_of_value 10, 70
