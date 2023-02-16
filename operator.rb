d040c024c95423b7ed016b7ac9ff3d98288bba14
amrendrakind <vision2010@gmail.com>
2023-02-16 09:36:04 +0530

    Operators uses

---
+++ operator.rb
@@ -0,0 +1,34 @@
+puts "AND Operator"
+var1 = true
+var2 = true
+puts var1 && var2
+
+var1 = true
+var2 = false
+puts var1 && var2
+
+var1 = false
+var2 = true
+puts var1 && var2
+
+var1 = false
+var2 = false
+puts var1 && var2
+
+
+puts "OR Operator"
+var1 = true
+var2 = true
+pp var1 || var2
+
+var1 = true
+var2 = false
+pp var1 || var2
+
+var1 = false
+var2 = true
+pp var1 || var2
+
+var1 = false
+var2 = false
+pp var1 || var2
