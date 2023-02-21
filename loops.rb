b4284fe092fcf7981df58bfe501a539282238b7f
amrendrakind <vision2010@gmail.com>
2023-02-21 09:55:05 +0530

    Random number guessing

--- loops.rb
+++ loops.rb
@@ -4,3 +4,16 @@ def mind_reader(range)
 end
 
 mind_reader(10)
+
+def mind_reader(range)
+    magic_number = rand(range)
+    print "What is your guess ? "
+    guess = gets.chomp.to_i
+    if guess == magic_number
+        puts "Awesome!!! That is right!"
+    else
+        puts "Sorry, that is not correct number. Currect Number is #{magic_number}."
+    end
+end
+
+mind_reader(20)
