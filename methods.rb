095762ba877922a26aaa534922e615a319f87776
amrendrakind <vision2010@gmail.com>
2023-02-27 00:56:48 +0530

    Methods manipulation

--- methods.rb
+++ methods.rb
@@ -9,3 +9,22 @@ def log_messages(**params)
 end
 
 log_messages(message: "This is the message to log",time: Time.now)
+
+puts "------------Any Number of Parameters-------------"
+
+def get_data(url, header = {}, *params)
+    puts "Calling #{url}"
+    if header.length > 0
+        puts "Headers: #{header}"
+    else
+        puts "No headers"
+    end
+    params.each do |param|
+        puts "Found params: #{param}"
+    end
+end
+url = "https://exampleapi.com"
+header = {accept: "application/json"}
+params = []
+params << "date=#{Time.now}"
+get_data(url, header, params)
