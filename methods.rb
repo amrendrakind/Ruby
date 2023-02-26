254cdf8e1b2aaeedd21eb6b8b8b939197bb3b974
amrendrakind <vision2010@gmail.com>
2023-02-27 00:02:10 +0530

    Methods manipulation

--- methods.rb
+++ methods.rb
@@ -3,3 +3,9 @@ def log(message, time = Time.now)
 end
 
 puts log "I am awesome!!"
+
+def log_messages(**params)
+    puts params.inspect
+end
+
+log_messages(message: "This is the message to log",time: Time.now)
