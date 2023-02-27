ad4739c01ca402eea1c2307f2323887e283c8e57
amrendrakind <vision2010@gmail.com>
2023-02-27 09:11:40 +0530

    Use of Class and Methods

--- class.rb
+++ class.rb
@@ -14,3 +14,24 @@ puts "My name is " + u.name
 p = User.new("Paul K")
 puts "Your name is " + p.name
 
+puts "---------Basic class initialization and setting name------------"
+
+class User
+    def initialize(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def set_nam new_name
+        @name = new_name
+    end
+end
+
+u = User.new("Amrendra K")
+puts "My name is "+u.name
+u.set_nam("Paul K")
+puts "New set name is "+u.name
+
