2a852668ff019cf8c6b738c2e43d85c5ea036e9a
amrendrakind <vision2010@gmail.com>
2023-03-14 11:04:37 +0530

    Searching through array of hash

--- play.rb
+++ play.rb
@@ -1,5 +1,20 @@
+users = [
+    {username: "Amrendra", password: "Kumar"},
+    {username: "Amar", password: "Sharma"},
+    {username: "Paul", password: "Daniel"},
+    {username: "Sushil", password: "Verma"},
+]
 attempts = 1
 while attempts < 4
+    print "Enter User Name: "
+    username = gets.chomp
+    print "Enter Password: "
+    password = gets.chomp
+    users.each do |user|
+        if user[:username] == username && user[:password] == password then
+            puts user
+        end
+    end
     puts "Press 'n' or 'N' to quit or any key to continue"
     input = gets.chomp.downcase
     break if input =='n'
