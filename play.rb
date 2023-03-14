c1980b1f90319ae55eaa892cafa949fc540ba4e5
amrendrakind <vision2010@gmail.com>
2023-03-14 11:25:11 +0530

    Searching through array of hash

--- play.rb
+++ play.rb
@@ -4,21 +4,25 @@ users = [
     {username: "Paul", password: "Daniel"},
     {username: "Sushil", password: "Verma"},
 ]
+
+def authenticate_user(username, password, users_list)
+    users_list.each do |user|
+        return user if user[:username] == username && user[:password] == password
+    end
+    return "Credentials are not correct!!"
+end
+
 attempts = 1
 while attempts < 4
     print "Enter User Name: "
     username = gets.chomp
     print "Enter Password: "
     password = gets.chomp
-    users.each do |user|
-        if user[:username] == username && user[:password] == password then
-            puts user
-        else
-            puts "Credientials were not correct"
-        end
-    end
+    authenticate = authenticate_user(username, password, users)
+    puts authenticate
     puts "Press 'n' or 'N' to quit or any key to continue"
     input = gets.chomp.downcase
     break if input =='n'
     attempts += 1
 end
+puts "You have exceeded number of attempts."
