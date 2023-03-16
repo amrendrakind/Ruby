04bdd586763062cc012022674524df3f29de3614
amrendrakind <vision2010@gmail.com>
2023-03-16 23:32:42 +0530

    module and mixing manipulation

--- student.rb
+++ student.rb
@@ -5,12 +5,21 @@ class Student
     @username
     @password
 
+    def first_name=(name)
+        @first_name = name
+    end
+
+    def first_name
+        @first_name
+    end
+    
     def to_s
         "First Name: #{@first_name}"
     end
 end
 
 amrendra = Student.new
+# amrendra.first_name("Amrendra")
+amrendra.first_name = "Amrendra K"
 puts amrendra
-# amrendra.first_name = "Amrendra"
-# puts amrendra
+puts amrendra.first_name
