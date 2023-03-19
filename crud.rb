07fbb2270d1b9bab7e34aec24c814a20e229981a
amrendrakind <vision2010@gmail.com>
2023-03-20 01:02:31 +0530

    Using module methods in class

--- crud.rb
+++ crud.rb
@@ -3,22 +3,22 @@ module Crud
     puts "Module Crud activated!!"
     my_password = BCrypt::Password.create("my password")
 
-    def self.create_hash_digest(password)
+    def create_hash_digest(password)
         BCrypt::Password.create(password)
     end
 
-    def self.verify_hash_digest(password)
+    def verify_hash_digest(password)
         BCrypt::Password.new(password)
     end
 
-    def self.create_secure_users(users_list)
+    def create_secure_users(users_list)
         users_list.each do | user |
             user[:password] = create_hash_digest(user[:password])
         end
         users_list
     end
 
-    def self.authenticate_user(username, password, users_list)
+    def authenticate_user(username, password, users_list)
         users_list.each do | user |
             if (user[:username] == username && verify_hash_digest(user[:password]) == password)
                 return user
--- student.rb
+++ student.rb
@@ -1,25 +1,28 @@
+require_relative 'crud'
+
 class Student
-    @first_name
-    @last_name
-    @email
-    @username
-    @password
+    include Crud
+    attr_accessor :first_name, :last_name, :email, :username, :password
 
-    def first_name=(name)
-        @first_name = name
+    def initialize(first_name, last_name, email, username, password)
+        @first_name = first_name,
+        @last_name = last_name,
+        @email = email,
+        @username = username,
+        @password = password
     end
-
-    def first_name
-        @first_name
+    def create_hash_digest(password)
+        @password = password
     end
-    
     def to_s
-        "First Name: #{@first_name}"
+        "First Name: #{@first_name}, Last Name: #{last_name}, User Name: #{username}, Email address: #{email}"
     end
 end
 
-amrendra = Student.new
-# amrendra.first_name("Amrendra")
-amrendra.first_name = "Amrendra K"
-puts amrendra
-puts amrendra.first_name
+amrendra = Student.new("Amrendra", "Kumar", "amrendrak.ind@gmail.com", "amrendrakind", "MyPassword")
+john = Student.new("John", "Kkenedy", "johnk.ind@gmail.com", "johnkind", "MyPassword")
+
+p amrendra
+
+hashed_password = amrendra.create_hash_digest(amrendra.password)
+p hashed_password
