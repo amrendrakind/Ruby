830eee669e49717913ae0c18596f4504d30bce85
amrendrakind <vision2010@gmail.com>
2023-03-25 22:33:23 +0530

    Module and mixing example

--- modules_mixins.rb
+++ modules_mixins.rb
@@ -1,4 +1,33 @@
-A = false
-B = true
-c = A and B
-puts c
+module Address
+    attr_accessor :address_line1, :address_line2, :city, :state, :postal_code, :country
+    def mailing_lebel
+        label = []
+        label << address_line1
+        label << address_line2
+        label << "#{city}, #{state}, #{postal_code}"
+        label << country
+        label.join("\n")
+    end
+end
+
+class User
+    include Address
+end
+
+class Building
+    include Address
+end
+
+u = User.new
+b = Building.new
+
+u.address_line1 = "123 Main Street"
+b.address_line1 = "987 Broadway"
+
+puts u.address_line1
+puts b.address_line1
+
+puts u.instance_variable_get("@address_line1")
+puts u.instance_variable_get("@address_line1").object_id
+puts b.instance_variable_get("@address_line1")
+puts b.instance_variable_get("@address_line1").object_id
