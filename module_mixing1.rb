b1bc0adae96d0bffa1be01090227faf42eb7d293
amrendrakind <vision2010@gmail.com>
2023-04-04 11:21:41 +0530

    Modle mising and method calling preference

---
+++ module_mixing1.rb
@@ -0,0 +1,15 @@
+module M
+    def report
+        puts "Report method in Module M"
+    end
+end
+class C 
+prepend M 
+    def report
+        puts "Report method in Class C"
+    end
+end
+
+obj = C.new
+puts obj.report
+
