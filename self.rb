74a117eae9d321cc5684de4c4beef7333edbbfcc
amrendrakind <vision2010@gmail.com>
2023-04-05 10:48:27 +0530

    Self object example

--- self.rb
+++ self.rb
@@ -7,4 +7,14 @@ class C
     end
     puts "Back in the outer level of C:"
     puts self
+
+    def x
+        puts "Class C, method x: "
+        puts self
+    end
 end
+
+c = C.new
+c.x
+puts "That was a call to x by:#{c}"
+
