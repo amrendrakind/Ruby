fbbc1d7e9401b5131e090b37cff5c51aaba3c07f
amrendrakind <vision2010@gmail.com>
2023-04-07 10:08:01 +0530

    Example of class variable

---
+++ class_var.rb
@@ -0,0 +1,40 @@
+class Car
+    @@makes = []
+    @@cars = {}
+    @@total_count = 0
+    attr_reader :make
+    
+    def initialize(make)
+        if @@makes.include?(make)
+            puts "Creating a new #{make}"
+            @make = make
+            @@cars[make] += 1
+            @@total_count += 1
+        else
+            raise "No such make: #{make}"
+        end
+    end
+
+    def self.total_count
+        @@total_count
+    end
+
+    def self.add_make(make)
+        unless @@makes.include?(make)
+            @@makes << make
+            @@cars[make] = 0
+        end
+    end
+
+    def make_mates
+        @@cars[self.make]
+    end
+end
+
+Car.add_make("Honda")
+Car.add_make("Ford")
+
+h = Car.new("Honda")
+f = Car.new("Ford")
+h2 = Car.new("Honda")
+
