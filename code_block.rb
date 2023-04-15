dda50d4e1c402792d572d2e24bb9e07a7a981cfd
amrendrakind <vision2010@gmail.com>
2023-04-15 23:48:04 +0530

    use of code block

---
+++ code_block.rb
@@ -0,0 +1,12 @@
+class Temperature
+    def c2f(celcious)
+        ft = (celcious*9/5)+32
+    end
+end
+
+t = Temperature.new
+celcious = [0,10,20,30,40,40,50,60,70,80,90,100]
+puts "Celcious \t Farenheit"
+for c in celcious
+    puts "#{c}\t #{t.c2f(c)}"
+end
