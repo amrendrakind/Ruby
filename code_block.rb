4ab2168e4e844e3c85ab933e76b27f5d915747b3
amrendrakind <vision2010@gmail.com>
2023-04-16 22:55:57 +0530

    use of code block

--- code_block.rb
+++ code_block.rb
@@ -6,6 +6,10 @@ end
 
 t = Temperature.new
 celcious = [0,10,20,30,40,40,50,60,70,80,90,100]
+for c in celcious
+    print "#{c} "
+end
+puts
 puts "Celcious \t Farenheit"
 for c in celcious
     puts "#{c}\t #{t.c2f(c)}"
