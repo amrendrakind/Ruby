90c53b4683db844f07d4a6f582f1ee54411cbb64
amrendrakind <vision2010@gmail.com>
2023-04-29 23:31:51 +0530

    Inventory update

--- guitar/inventory.rb
+++ guitar/inventory.rb
@@ -1,5 +1,13 @@
 class Inventory
     guitars = []
+    serial_number = 123, 
+    price = 5000,
+    builder = 123,
+    model = "Awesome",
+    type = "sinle_hand",
+    back_wood = "Metal",
+    top_wood = "Wood"
+    guitar = Guitar.new(serial_number, price, builder, model, type, back_wood, top_wood)
     add_guitar(guitar)
     get_guitar(guitar)
     search_guitar(guitar)
