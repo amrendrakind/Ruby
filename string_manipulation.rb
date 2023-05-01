19b5836e8e9fb808e492fa91ac18ff5c8b7b84bc
amrendrakind <vision2010@gmail.com>
2023-05-01 12:07:42 +0530

    String manipulation

---
+++ string_manipulation.rb
@@ -0,0 +1,15 @@
+str = "Ruby is a cool Language"
+puts str[5]
+puts str[15]
+puts str[-15]
+puts str[5,10]
+puts str[5..10]
+str = "Ruby is a great Language"
+puts str.include?("great")
+
+puts str.start_with?"Ruby"
+puts str.start_with?"Muby"
+puts str.end_with?"Language"
+puts str.end_with?"age!!"
+puts str.empty?
+puts "".empty?
