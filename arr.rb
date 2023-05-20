87124e8bd3b76faf3d3d0e60d55d8b0f7ec21831
amrendrakind <vision2010@gmail.com>
2023-05-20 21:44:04 +0530

    Array manipulations

--- arr.rb
+++ arr.rb
@@ -13,3 +13,19 @@ z.select {|number| print number.to_s + " "  if number.odd? }
 p z.methods
 p z
 p z.reverse
+
+str = "aeiou"
+p str.split(//)
+pp %w(I am a hustler)
+
+pp arr = [1,2,3,4,5,6,7,8,9,10]
+puts "removing from begining of array using shift" 
+a = pp arr.shift
+pp arr
+puts "removing from end of array using pop"
+pp b = arr.pop
+pp arr
+puts "Interting element at begining of array using unshift" 
+pp arr.unshift(a)
+puts "Interting element at the end of array using push" 
+pp arr.push(b)
