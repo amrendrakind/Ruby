9b698a427d4531ec9e683ec18e4343a4a3c32831
amrendrakind <vision2010@gmail.com>
2023-05-29 11:45:38 +0530

    Enumerable examples

---
+++ enumerable.rb
@@ -0,0 +1,5 @@
+a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+pp a.find_all { |item| item > 10 }
+pp a.select { |item| item < 10 }
+pp a.select { |item| item%2 == 0 }
+pp a.reject { |item| item%2 == 0 }
