31e91a9106cd32ce90035a6013f522f6635ca749
amrendrakind <vision2010@gmail.com>
2023-05-29 12:40:09 +0530

    Enumerable examples

--- enumerable.rb
+++ enumerable.rb
@@ -3,3 +3,6 @@ pp a.find_all { |item| item > 10 }
 pp a.select { |item| item < 10 }
 pp a.select { |item| item%2 == 0 }
 pp a.reject { |item| item%2 == 0 }
+
+colors = %w { red orrange yellow green blue indigo violet }
+
