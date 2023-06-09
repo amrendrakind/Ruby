f9f7ea56935a54957f52a591363fa170bed12727
amrendrakind <vision2010@gmail.com>
2023-06-09 10:55:21 +0530

    Data filter with GREP

--- enumerable.rb
+++ enumerable.rb
@@ -8,3 +8,8 @@ colors = %w{ red orrange yellow green blue indigo violet }
 pp colors
 pp colors.grep(/o/) { |color| color.capitalize }
 
+all_data = [75, "hello", 10..20, "goodbye"]
+
+pp all_data.grep(String)
+pp all_data.grep(Integer)
+pp all_data.grep(Range)
