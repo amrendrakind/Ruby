349d559e8284b08641e82e760602f6819ac743f9
amrendrakind <vision2010@gmail.com>
2023-06-27 09:10:15 +0530

    Enumerables examples

--- enumerable.rb
+++ enumerable.rb
@@ -8,11 +8,11 @@ colors = %w{ red orrange yellow green blue indigo violet }
 pp colors
 pp colors.grep(/o/) { |color| color.capitalize }
 
+pp colors.group_by{ |color| color.size }
+
 all_data = [75, "hello", 10..20, "goodbye"]
 
 pp all_data.grep(String)
 pp all_data.grep(Integer)
 pp all_data.grep(Range)
-pp all_data.grep(String)
-pp all_data.grep(Integer)
-pp all_data.grep(Range)
+
